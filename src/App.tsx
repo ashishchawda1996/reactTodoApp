ee109afc97ea0ea11c1ad0c4c9d3a73401ab1c54
Ashish <ashishchawda1122@gmail.com>
2023-10-05 15:36:43 +0530

    add filter task function in todo-list

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import AddTask from './components/add-task/add-task.component';
 
 import './App.css';
 import TodoList from './components/todo-list/todo-list.component';
+import Navbar from './components/navbar/navbar.component';
 
 function App() {
   return (
     <div className='main'>
       <h1>REACT TODO APPLICATION</h1>
       <AddTask />
+      <Navbar />
       <TodoList />
     </div>
   );
--- src/components/todo-list/todo-list.component.tsx
+++ src/components/todo-list/todo-list.component.tsx
@@ -1,19 +1,31 @@
 import { Fragment } from "react"
 import { useTodos } from "../../contexts/todo.context"
+import { useSearchParams } from "react-router-dom"
 
 import './todo-list.styles.css'
 
 const TodoList = () => {
     const { todos, todoAsCompleted, handleDeleteTodo } = useTodos()
+    const [ searchParams ] = useSearchParams()
+    let navStatus = searchParams.get('todos')
+    let filterTodos = todos;
+
+    if (navStatus==='active') {
+      filterTodos = filterTodos.filter((todo) => !todo.completed)
+    }else if (navStatus==='completed') {
+      filterTodos = filterTodos.filter((todo) => todo.completed)
+    }
+    
 
     const handleChange = (id:string) => {
         todoAsCompleted(id) 
     }
+
   return (
     <Fragment>
       <ul className="main-task">
         {
-            todos.map((todo) => {
+            filterTodos.map((todo) => {
                 const date = new Date(todo.createdAt)
                 return <li key={todo.id}>
                         <input type="checkbox" id={`task-${todo.id}`} onChange={event=>handleChange(todo.id)} checked={todo.completed} />
